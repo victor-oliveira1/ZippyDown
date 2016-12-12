2e2d6a50d2c3a744f51883ad6502c9dd8bab6180
Victor Oliveira <victor.oliveira@gmx.com>
2016-12-12 10:58:06 -0200

    Add files via upload
    Agora o tamanho do arquivo também é informado.

--- zippyshare_downloader.sh
+++ zippyshare_downloader.sh
@@ -8,10 +8,13 @@ var_b=$(echo "$site"|grep -Eo var\ b\ \=\ [0-9]\+|cut -d ' ' -f4)
 var_a_div=$(($var_a/3))
 var_final=$(($var_a+$var_a_div%$var_b))
 
+echo $var_a $var_b $var_final
+
 url=$(echo $1|sed 's\http://\\g'|cut -d '/' -f1)
 id=$(echo $1|sed 's\http://\\g'|cut -d '/' -f3)
 arq=$(echo "$site"|grep 'twitter:title'|cut -d '"' -f4)
+tam=$(echo "$site"|grep Size\:|cut -d\> -f4|cut -d\< -f1)
 
-echo "Baixando: $arq"
+echo -e "Baixando: $arq\tTamanho: $tam"
 
 wget -q --show-progress "http://$url/d/$id/$var_final/$arq"
