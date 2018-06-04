176bdd8cf567a14903e7c1e1e7f5dcec84d0754e
Victor Oliveira <victor.oliveira@gmx.com>
2018-06-04 15:32:19 -0300

    Update zippyshare_downloader.py
    Testing new way to process algorithm.

--- zippyshare_downloader.py
+++ zippyshare_downloader.py
@@ -3,75 +3,22 @@
 #This work is free. You can redistribute it and/or modify it under the
 #terms of the Do What The Fuck You Want To Public License, Version 2,
 #as published by Sam Hocevar. See the COPYING file for more details.
+#!/bin/python3
+#Copyright (C) 2018 Victor Oliveira <victor.oliveira@gmx.com>
+#This work is free. You can redistribute it and/or modify it under the
+#terms of the Do What The Fuck You Want To Public License, Version 2,
+#as published by Sam Hocevar. See the COPYING file for more details.
 
-from urllib import request
-from urllib import parse
-import re
-import os
-import argparse
-from html.parser import HTMLParser
+from selenium import webdriver
 
-BUFFER = 1024 * 8
+#JUST A TEST FILE
+URL = 'http://www14.zippyshare.com/pd/GDjSDTr2/40276/fifa-mobile-soccer-v8.1.00-mod.apk'
 
-class MyHTMLParser(HTMLParser):
-    def __init__(self):
-        HTMLParser.__init__(self)
-    def handle_data(self, data):
-        if 'dlbutton' in data:
-            self.data = data
+def Download():
+    
 
-def URLConvert(url, auth_number, filename):
-    str_convert = {'/v/' : '/d/',
-                   'file.html' : str(auth_number)}
-    for old, new in str_convert.items():
-        url = url.replace(old, new)
-    url += '/' + filename
-    return url
-
-def dl(url):
-    if not 'zippyshare.com/v/' in url:
-        print('Invalid URL')
-        exit(1)
-
-    print('Starting Zippyshare Downloader for %s...'%url)
-
-    html_page = request.urlopen(url).read().decode()
-    html_parser = MyHTMLParser()
-    html_parser.feed(html_page)
-
-    download_link = re.findall('/d/.*;', html_parser.data)
-    auth_number = eval(re.findall('\(.*\)', download_link[0])[0])
-    filename_encoded = re.findall('"/.*"', download_link[0])[0].strip('"/')
-    filename = parse.unquote(filename_encoded)
-
-    url_download = URLConvert(url, auth_number, filename_encoded)
-    req_download = request.urlopen(url_download)
-    filesize = int(req_download.getheader('Content-Length'))
-    print('Downloading: {}'.format(filename))
-    print('Size: {:.2f}MB'.format(filesize / 1000 / 1000))
-
-    with open(filename, 'wb') as file:
-        c = 0
-        while True:
-            tmp = req_download.read(BUFFER)
-            if tmp:
-                file.write(tmp)
-                c += 1
-                print('{:.1f}%'.format((c * BUFFER / filesize) * 100), end='\r')
-            else:
-                break
-
-if __name__ == "__main__":
-    args_parser = argparse.ArgumentParser()
-    args_parser.add_argument('url',
-                             help='URL or file with URLs to download')
-    args = args_parser.parse_args()
-    url = args.url
-
-    if os.path.isfile(url):
-        with open(url) as fp:
-            for line in fp:
-                dl(line.rstrip())
-    else:
-        dl(url)
-
+driver = webdriver.PhantomJS()
+driver.get(URL)
+element = driver.find_element_by_id('dlbutton')
+url_download = element.get_attribute('href')
+print(url_download)
